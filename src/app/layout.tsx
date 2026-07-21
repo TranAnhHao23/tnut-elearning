25e4a416e426c422d795b4262ca9ad33d99f7504
TranAnhHao23 <trananhhaonuce@gmail.com>
2026-07-21 22:54:13 +0700

    update gTag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
@@ -18,6 +19,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="vi">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtm.js?id=GTM-PZL7WJH6"
+          strategy="afterInteractive"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          `}
+        </Script>
+      </head>
       <body className="antialiased flex flex-col min-h-screen">
         <Header />
         <main className="flex-grow">
